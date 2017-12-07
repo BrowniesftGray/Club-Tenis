ee63a1ed553a8a996c9db71a38484af88363ca8d
ImTheGray <graybohemia@gmail.com>
2017-12-07 20:17:36 +0100

    Resultados.

--- php/Listados/listadoResultados.php
+++ php/Listados/listadoResultados.php
@@ -4,9 +4,10 @@ if (!isset($_POST['btnAceptar'])) {
 
 }
 else{
-  $id = $_REQUEST['txtIdEvento'];
+  $idCompeticion = $_REQUEST['IdEvento'];
   $usuario = 'root';
   $contraseña = '';
+
   try {
     $con = new PDO('mysql:host=localhost;dbname=club', $usuario, $contraseña);
     $mbd = null;
@@ -15,17 +16,82 @@ else{
       die();
   }
 
-  //Ganador:(Select jugadores.nombreJugador, jugadores.idJugador, competiciones.nombreEvento from ((resultados INNER JOIN jugadores on resultados.idGanador = jugadores.idJugador) INNER JOIN competiciones on resultados.idCompeticionFK = competiciones.idCompeticion) WHERE idCompeticion = $id");
-  //Perdedor:(Select jugadores.nombreJugador, jugadores.idJugador, competiciones.nombreEvento from ((resultados INNER JOIN jugadores on resultados.idPerdedor = jugadores.idJugador) INNER JOIN competiciones on resultados.idCompeticionFK = competiciones.idCompeticion) WHERE idCompeticion = $id");
-  //"SELECT COUNT(resultados.idGanador), COUNT(resultados.idPerdedor), competiciones.nombreEvento, jugadores.nombreJugador FROM ((resultados WHERE idCompeticionFK = $idCompeticion) INNER JOIN jugadores ON (idPerdedor = $id OR idGanador = $id) = jugadores.idJugador) WHERE idCompeticionFK = $id");
-  $sql = $con->prepare("SELECT COUNT(resultados.idGanador), COUNT(resultados.idPerdedor), competiciones.nombreEvento, jugadores.nombreJugador FROM ((resultados INNER JOIN competiciones ON resultados.idCompeticionFK = competiciones.idCompeticion) INNER JOIN jugadores ON resultados.idPerdedor = jugadores.idJugador OR resultados.idGanador = jugadores.idJugador) WHERE idCompeticionFK = $id");
-  $sql->execute();
+  //Resultados para la competición
+  $participantes = array();
 
-  while ($row = $sql->fetchAll(PDO::FETCH_ASSOC)) {
-    //Se almacena en un array las filas que se vaya leyendo
-    $data[] = $row;
+  //Obtenemos ganadores
+  $obtenerGanadores = $con->prepare("SELECT DISTINCT idGanador FROM resultados WHERE idCompeticionFK=$idCompeticion ORDER BY COUNT(idGanador)");
+  $obtenerGanadores->execute();
+  $ganadores = $obtenerGanadores->fetchAll(PDO::FETCH_ASSOC);
+  for ($i=0; $i < count($ganadores); $i++) {
+    $participantes[] = $ganadores[$i]['idGanador'];
   }
 
-  echo json_encode($data);
+
+  //obtenemos Perdedores
+  $obtenerPerdedores = $con->prepare("SELECT DISTINCT idPerdedor FROM resultados WHERE idCompeticionFK=$idCompeticion");
+  $obtenerPerdedores->execute();
+  $perdedores = $obtenerPerdedores->fetchAll(PDO::FETCH_ASSOC);
+  //print_r($perdedores);
+  for ($i=0; $i < count($perdedores); $i++) {
+    $perdedor = $perdedores[$i]['idPerdedor'];
+    if (!in_array($perdedor, $participantes)) {
+      $participantes[] = $perdedores[$i]['idPerdedor'];
+    }
+  }
+
+
+  echo "<div class='row' style='padding-top: 1rem'><table class='table table-bordered'>";
+  echo "<thead>";
+    echo "<tr>";
+    echo "<th>";
+      echo "Participante";
+    echo "</th>";
+    echo "<th>";
+      echo "Victorias";
+    echo "</th>";
+    echo "<th>";
+      echo "Derrotas";
+    echo "</th>";
+    echo "</tr>";
+  echo "</thead>";
+
+  for ($i=0; $i < count($participantes); $i++) {
+    $participante = $participantes[$i];
+    $victorias = $con->prepare("SELECT COUNT(idGanador) FROM resultados WHERE idCompeticionFK = $idCompeticion AND idGanador = $participante");
+    $victorias->execute();
+    $derrotas = $con->prepare("SELECT COUNT(idPerdedor) FROM resultados WHERE idCompeticionFK = $idCompeticion AND idPerdedor = $participante");
+    $derrotas->execute();
+
+    $nombreJugador = $con->prepare("SELECT nombreJugador FROM jugadores WHERE idJugador = $participante");
+    $nombreJugador->execute();
+    
+    $row = $victorias->fetchAll(PDO::FETCH_ASSOC);
+    $data[$i]['Victorias'] = $row[0]['COUNT(idGanador)'];
+
+    $row = $derrotas->fetchAll(PDO::FETCH_ASSOC);
+    $data[$i]['Derrotas'] = $row[0]['COUNT(idPerdedor)'];
+
+    $row = $nombreJugador->fetchAll(PDO::FETCH_ASSOC);
+    $data[$i]['Nombre'] = $row[0]['nombreJugador'];
+
+
+  }
+  array_multisort($data, SORT_DESC);
+  for ($i=0; $i < count($data); $i++) {
+    echo "<tr>";
+      echo "<td>";
+      echo $data[$i]['Nombre'];
+      echo "</td>";
+      echo "<td>";
+      echo $data[$i]['Victorias'];
+      echo "</td>";
+      echo "<td>";
+      echo $data[$i]['Derrotas'];
+      echo "</td>";
+    echo "</tr>";
+  }
+  echo "</table></div>";
+
 }
 ?>
--- procesos/resultados.php
+++ procesos/resultados.php
@@ -247,8 +247,87 @@ session_start();
                 </div>
             </div>
         </form>
-    </div>
     <?php
+
+    //Resultados para la competición
+    $participantes = array();
+
+    //Obtenemos ganadores
+    $obtenerGanadores = $con->prepare("SELECT DISTINCT idGanador FROM resultados WHERE idCompeticionFK=$idCompeticion ORDER BY COUNT(idGanador)");
+    $obtenerGanadores->execute();
+    $ganadores = $obtenerGanadores->fetchAll(PDO::FETCH_ASSOC);
+    for ($i=0; $i < count($ganadores); $i++) {
+      $participantes[] = $ganadores[$i]['idGanador'];
+    }
+
+
+    //obtenemos Perdedores
+    $obtenerPerdedores = $con->prepare("SELECT DISTINCT idPerdedor FROM resultados WHERE idCompeticionFK=$idCompeticion");
+    $obtenerPerdedores->execute();
+    $perdedores = $obtenerPerdedores->fetchAll(PDO::FETCH_ASSOC);
+    //print_r($perdedores);
+    for ($i=0; $i < count($perdedores); $i++) {
+      $perdedor = $perdedores[$i]['idPerdedor'];
+      if (!in_array($perdedor, $participantes)) {
+        $participantes[] = $perdedores[$i]['idPerdedor'];
+      }
+    }
+
+
+    echo "<div class='row' style='padding-top: 1rem'><table class='table table-bordered'>";
+    echo "<thead>";
+      echo "<tr>";
+      echo "<th>";
+        echo "Participante";
+      echo "</th>";
+      echo "<th>";
+        echo "Victorias";
+      echo "</th>";
+      echo "<th>";
+        echo "Derrotas";
+      echo "</th>";
+      echo "</tr>";
+    echo "</thead>";
+
+    for ($i=0; $i < count($participantes); $i++) {
+      $participante = $participantes[$i];
+      $victorias = $con->prepare("SELECT COUNT(idGanador) FROM resultados WHERE idCompeticionFK = $idCompeticion AND idGanador = $participante");
+      $victorias->execute();
+      $derrotas = $con->prepare("SELECT COUNT(idPerdedor) FROM resultados WHERE idCompeticionFK = $idCompeticion AND idPerdedor = $participante");
+      $derrotas->execute();
+
+      $nombreJugador = $con->prepare("SELECT nombreJugador FROM jugadores WHERE idJugador = $participante");
+      $nombreJugador->execute();
+      //$sql = $con->prepare("SELECT COUNT(resultados.idGanador), resultados.idGanador, COUNT(resultados.idPerdedor), resultados.idPerdedor, competiciones.nombreEvento, jugadores.nombreJugador FROM ((resultados INNER JOIN competiciones ON resultados.idCompeticionFK = competiciones.idCompeticion) INNER JOIN jugadores ON resultados.idPerdedor = jugadores.idJugador OR resultados.idGanador = jugadores.idJugador) WHERE idCompeticionFK = $idCompeticion AND idGanador=$participante");
+      //$sql->execute();
+
+      $row = $victorias->fetchAll(PDO::FETCH_ASSOC);
+      $data[$i]['Victorias'] = $row[0]['COUNT(idGanador)'];
+
+      $row = $derrotas->fetchAll(PDO::FETCH_ASSOC);
+      $data[$i]['Derrotas'] = $row[0]['COUNT(idPerdedor)'];
+
+      $row = $nombreJugador->fetchAll(PDO::FETCH_ASSOC);
+      $data[$i]['Nombre'] = $row[0]['nombreJugador'];
+
+
+    }
+    array_multisort($data, SORT_DESC);
+    for ($i=0; $i < count($data); $i++) {
+      echo "<tr>";
+        echo "<td>";
+        echo $data[$i]['Nombre'];
+        echo "</td>";
+        echo "<td>";
+        echo $data[$i]['Victorias'];
+        echo "</td>";
+        echo "<td>";
+        echo $data[$i]['Derrotas'];
+        echo "</td>";
+      echo "</tr>";
+    }
+    echo "</table></div>";
+
   }
   else{
     echo '<div class="alert alert-warning alert-dismissable" role="alert">No estaba inscrito a esta competición, <a href="../eventos.php">vuelva a Eventos</a>.</div>';
@@ -273,5 +352,6 @@ session_start();
     return $con;
   }
     ?>
+  </div>
   </body>
   </html>
